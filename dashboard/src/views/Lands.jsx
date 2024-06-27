9a076f84e2b363b0879626c7381fde70184b677f
MarcGoulding <mgtojar@gmail.com>
2024-06-27 10:51:23 +0200

    v3

--- dashboard/src/views/Lands.jsx
+++ dashboard/src/views/Lands.jsx
@@ -1,10 +1,52 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Box, Heading, Text, SimpleGrid, Flex } from '@chakra-ui/react';
 import { Line, Pie } from 'react-chartjs-2';
 import '../chartConfig';
-import { lineData, samplePieData, options } from '../data/charts';
+import { lineData, options, samplePieData } from '../data/charts';
 
-export default function Lands({lands}) {
+export default function Lands({ lands }) {
+  const [pieData, setPieData] = useState(samplePieData);
+
+  useEffect(() => {
+    if (lands.length > 0) {
+      const tierCounts = lands.reduce((acc, land) => {
+        const tier = land.metadata?.tier || 'Unknown';
+        if (!acc[tier]) acc[tier] = 0;
+        acc[tier] += 1;
+        return acc;
+      }, {});
+
+      const pieLabels = Object.keys(tierCounts);
+      const pieValues = Object.values(tierCounts);
+
+      setPieData({
+        labels: pieLabels,
+        datasets: [
+          {
+            data: pieValues,
+            backgroundColor: [
+              '#FF6384',
+              '#36A2EB',
+              '#FFCE56',
+              '#FF9F40',
+              '#FFCD56',
+              '#4BC0C0',
+              '#9966FF',
+            ],
+            hoverBackgroundColor: [
+              '#FF6384',
+              '#36A2EB',
+              '#FFCE56',
+              '#FF9F40',
+              '#FFCD56',
+              '#4BC0C0',
+              '#9966FF',
+            ],
+          },
+        ],
+      });
+    }
+  }, [lands]);
 
   return (
     <Box p={5}>
@@ -34,15 +76,15 @@ export default function Lands({lands}) {
         </Box>
         <Box bg="gray.100" p={5} borderRadius="md">
           <Text fontSize="2xl">Distribution of Lands for Sale</Text>
-          <Pie data={samplePieData} options={options} />
+          <Pie data={pieData} options={options} />
         </Box>
         <Box bg="gray.100" p={5} borderRadius="md">
           <Text fontSize="2xl">Market Cap of Lands</Text>
-          <Pie data={samplePieData} options={options} />
+          <Pie data={pieData} options={options} />
         </Box>
         <Box bg="gray.100" p={5} borderRadius="md">
           <Text fontSize="2xl">Average Price for Sale of Lands</Text>
-          <Pie data={samplePieData} options={options} />
+          <Pie data={pieData} options={options} />
         </Box>
       </SimpleGrid>
       <Flex mt={10} direction="column" gap={10}>
